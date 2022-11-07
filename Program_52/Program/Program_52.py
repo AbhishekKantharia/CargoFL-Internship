d8a8347f456efdb985e8494c12eaeaeda5ddae09
AbhishekKantharia <61312706+AbhishekKantharia@users.noreply.github.com>
2022-11-07 12:31:43 +0530

    Delete Program_52 directory

--- Program_52/Program/Program_52.py
+++
@@ -1,5 +0,0 @@
-# Import wikipedia module using the import keyword.
-import wikipedia 
-# Pass the number of pages as an argument to the random() function to get some 
-# random titles and print it
-print(wikipedia.random(pages=8))
