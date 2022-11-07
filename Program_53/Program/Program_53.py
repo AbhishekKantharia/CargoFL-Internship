4cde7e1d9c1161733146f31252df15458e4030b3
AbhishekKantharia <61312706+AbhishekKantharia@users.noreply.github.com>
2022-11-07 12:31:52 +0530

    Delete Program_53 directory

--- Program_53/Program/Program_53.py
+++
@@ -1,8 +0,0 @@
-# Print the grinning face emoji
-print("\U0001f600")
- 
-#  grinning squinting face emoji
-print("\U0001F606")
-print("\U0001F649")
-# Print the rolling on the floor laughing emoji
-print("\U0001F923")
