9c6fb24ba2c94ae8b2a07f2b43cd4533826b7de8
AbhishekKantharia <61312706+AbhishekKantharia@users.noreply.github.com>
2022-11-07 13:01:27 +0530

    Delete Program_4/Program directory

--- Program_4/Program/App/Admin.py
+++
@@ -1,9 +0,0 @@
-from App.Actor import Actor
-
-class Admin(Actor):
-	admin = {}
-	
-	def __init__(self, AdminDAO):
-		self.sess_key = "admin"
-		self.dao = AdminDAO
-		self.route_url = "/admin/"
--- Program_4/Program/Models/DAO.py
+++
@@ -1,5 +0,0 @@
-from Models.DBDAO import DBDAO
-
-class DAO():
-	def __init__(self, app):
-		self.db = DBDAO(app)
