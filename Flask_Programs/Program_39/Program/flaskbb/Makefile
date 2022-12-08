5f21f39f741738556bd132b45a22c66b3f3cf447
AbhishekKantharia <61312706+AbhishekKantharia@users.noreply.github.com>
2022-12-08 15:57:02 +0530

    Add files via upload

---
+++ Flask_Programs/Program_39/Program/flaskbb/Makefile
@@ -0,0 +1,48 @@
+.PHONY: clean install help test lint isort run dependencies docs wheel upload
+.DEFAULT_GOAL := help
+
+help: ## Displays this help message.
+	@awk 'BEGIN {FS = ":.*?## "} /^[a-zA-Z_-]+:.*?## / {printf "\033[36m%-15s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
+
+
+dependencies:requirements.txt
+	@echo "Installing dependencies..."
+	@pip install -r requirements.txt 1>/dev/null
+
+clean: ## Remove unwanted stuff such as __pycache__, etc...
+	find . -name '*.pyc' -exec rm -f {} +
+	find . -name '*.pyo' -exec rm -f {} +
+	find . -name '*~' -exec rm -f {} +
+	find . -name '__pycache__' -exec rm -rf {} +
+
+test: ## Runs the testsuite
+	tox
+
+run: ## Runs the development server with the development config
+	flaskbb run --debugger --reload
+
+frontend: ## Runs the webpack server which watches for changes in flaskbb/themes/aurora
+	cd flaskbb/themes/aurora && npm run watch
+
+devconfig:dependencies ## Generates a development config
+	flaskbb makeconfig -d
+
+install:dependencies ## Installs the dependencies and FlaskBB
+	flaskbb install
+
+docs: ## Builds the Sphinx docs
+	$(MAKE) -C docs html
+
+lint: ## Checks the source for style errors
+	@type flake8 >/dev/null 2>&1 || echo "Flake8 is not installed. You can install it with 'pip install flake8'."
+	flake8
+
+isort:  ## Sorts the python imports
+	@type isort >/dev/null 2>&1 || echo "isort is not installed. You can install it with 'pip install isort'."
+	isort --order-by-type -rc -up
+
+dist: ## Creates distribution packages (bdist_wheel, sdist)
+	python setup.py sdist bdist_wheel
+
+upload:dist ## Uploads a new version of FlaskBB to PyPI
+	twine upload --skip-existing dist/*
---
+++ Flask_Programs/Program_39/Program/flaskbb/docs/conf.py
@@ -0,0 +1,104 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("_themes"))
+
+
+# -- Project information -----------------------------------------------------
+project = u'FlaskBB'
+copyright = u'2021, Peter Justin'
+author = u'Peter Justin'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.intersphinx",
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "alabaster"
+
+html_theme_options = {
+    'logo': 'logo-full.png',
+    'github_banner': True,
+    'github_user': 'sh4nks',
+    'github_repo': 'flaskbb',
+    'github_type': 'star',
+    'description': ("FlaskBB is a simple and extensible forum software "
+                    "for building communities."),
+    'fixed_sidebar': True,
+    'show_related': True
+}
+
+# The name for this set of Sphinx documents.
+# "<project> v<release> documentation" by default.
+#
+html_title = u'FlaskBB Documentation'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
+
+# Custom sidebar templates, maps document names to template names.
+html_sidebars = {
+    'index': [
+        'about.html',
+        'sidebarintro.html',
+        'sourcelink.html',
+        'searchbox.html'
+    ],
+    '**': [
+        'about.html',
+        'localtoc.html',
+        'relations.html',
+        'sourcelink.html',
+        'searchbox.html'
+    ]
+}
+html_show_sourcelink = False
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'flask': ('https://flask.palletsprojects.com/en/latest/', None),
+    'werkzeug': ('https://werkzeug.palletsprojects.com/en/latest/', None),
+    'click': ('https://click.palletsprojects.com/en/latest/', None),
+    'jinja': ('https://jinja.palletsprojects.com/en/latest/', None),
+    'wtforms': ('https://wtforms.readthedocs.io/en/master/', None),
+}
+
+autodoc_member_order = 'bysource'
+
+# due to rtd using sphinx < 2
+root_doc = master_doc = 'index'
